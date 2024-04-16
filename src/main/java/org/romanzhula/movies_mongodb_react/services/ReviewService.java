b3ceca71de25a68771e6b96caa04f2b1ae6e8542
RoMANzhula <roman1985al@ukr.net>
2024-04-16 15:46:08 +0300

    Add model Review, repository, service, controller and method to create review for movie

---
+++ src/main/java/org/romanzhula/movies_mongodb_react/services/ReviewService.java
@@ -0,0 +1,35 @@
+package org.romanzhula.movies_mongodb_react.services;
+
+import org.romanzhula.movies_mongodb_react.models.Movie;
+import org.romanzhula.movies_mongodb_react.models.Review;
+import org.romanzhula.movies_mongodb_react.repositories.ReviewRepository;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    private final MongoTemplate mongoTemplate;
+
+    public ReviewService(ReviewRepository reviewRepository, MongoTemplate mongoTemplate) {
+        this.reviewRepository = reviewRepository;
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first()
+        ;
+
+        return review;
+    }
+
+}
